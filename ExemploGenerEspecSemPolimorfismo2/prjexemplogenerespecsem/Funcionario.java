d848eb67302acb030c63043aa8f7e18db7661d8d
NicolasNFF <79235449+NicolasNFF@users.noreply.github.com>
2022-04-06 10:57:03 -0300

    Update Funcionario.java

--- ExemploGenerEspecSemPolimorfismo2/prjexemplogenerespecsem/Funcionario.java
+++ ExemploGenerEspecSemPolimorfismo2/prjexemplogenerespecsem/Funcionario.java
@@ -1,1 +1,35 @@
 
+package fatec.poo.model;
+
+/**
+ *
+ * @author Nicolas
+ */
+public abstract class Funcionario {
+    private int registro;
+    private String nome;
+    private String dtAdmissao;
+
+    public Funcionario(int registro, String nome, String dtAdmissao) {
+        this.registro = registro;
+        this.nome = nome;
+        this.dtAdmissao = dtAdmissao;
+    }
+    
+    // Metodo abstrato
+    // Apresenta apenas a assinatura
+   abstract public double calcSalBruto();
+    
+   
+   
+   public double calcDesconto(){
+       return(0.10 * calcSalBruto());
+   }
+   
+   public double calcSalLiquido(){
+       return(calcSalBruto() - calcDesconto());
+   }
+           
+    
+}
+
