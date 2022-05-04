4be1e0e44fd06ab030f1417e40422c33d0b2fb81
NicolasNFF <79235449+NicolasNFF@users.noreply.github.com>
2022-05-04 12:49:53 -0300

    Update Departamento.java

--- prjExemploAssociacaoBinaria/src/fatec/poo/model/Departamento.java
+++ prjExemploAssociacaoBinaria/src/fatec/poo/model/Departamento.java
@@ -3,15 +3,19 @@ package fatec.poo.model;
 
 /**
  *
- * @author Dimas
+ * @author Nicolas
  */
 public class Departamento {
     private String sigla;
     private String nome;
-
+    private Funcionario funcionarios[]; //representa a multiplicidade 1..*
+    private int numFunc;
+    
     public Departamento(String sigla, String nome) {
         this.sigla = sigla;
         this.nome = nome;
+        funcionarios = new Funcionario[10];
+        numFunc = 0;
     }
 
     public String getSigla() {
@@ -20,5 +24,22 @@ public class Departamento {
 
     public String getNome() {
         return nome;
+    }
+    
+    public void addFuncionario(Funcionario f){
+        funcionarios[numFunc]= f;
+        numFunc = numFunc + 1; // ou numFunc++ = soma , numFunc-- tira 1
+        
+    }
+    
+    public void listarFuncionarios(){
+        System.out.println("\n\nSigla: " + sigla);
+        System.out.println("Nome: " + nome);
+        System.out.println("\nRegistro\tNome\tCargo");
+        for (int x = 0; x < numFunc; x++) {
+            System.out.println(funcionarios[x].getRegistro() + "\t");
+            System.out.println(funcionarios[x].getNome() + "\t");
+            System.out.println(funcionarios[x].getCargo());
+        }
     }
 }
